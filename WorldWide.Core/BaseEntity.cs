334df0653b0957d595d990f778cf35c7bd3e1031
alex <alex@thinknature.gr>
2019-04-19 15:46:18 +0300

    First commit

---
+++ WorldWide.Core/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WorldWide.Core
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+        public int LastEditedBy { get; set; }
+        public DateTime ValidFrom { get; set; }
+        public DateTime ValidTo { get; set; }
+    }
+}
---
+++ WorldWide.Core/Domain/Application/User.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace WorldWide.Core.Domain.Application
+{
+    public class User : IdentityUser
+    {
+        public string FullName { get; set; }
+        public string PreferredName { get; set; }
+        public bool IsEmployee { get; set; }
+        public bool IsSalesperson { get; set; }
+        public string UserPreferences { get; set; }
+    }
+}
